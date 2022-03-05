e1c74c16cebfeccdf8af13ed0ecd8e0db20ce045
Windows <cisneros.jorge.a@gmail.com>
2022-03-04 21:58:02 -0600

    updated profile and x

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -5,5 +5,5 @@ Start-Job -FilePath $home\Documents\WindowsPowerShell\threads.ps1
 Function Prompt {"`n:)~ "}
 Function lf {Get-Command -Module X}
 Function cc {Set-Location $home}
-Function me {Set-Location "$home\Documents\WindowsPowerShell"}
+Function power {Set-Location "$home\Documents\WindowsPowerShell"}
 Function work {Set-Location "$home\OneDrive\Work"}
--- X.psm1
+++ X.psm1
@@ -1,3 +1,14 @@
+# Variables
+$GOOGLE = "https://www.google.com/search?q="
+$MUSIC = "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Chrome Apps\YouTube Music"
+$TEXT = "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Chrome Apps\Messages"
+$REASON = "C:\Program Files\Propellerhead\Reason 9\Reason"
+$ONENOTE = "C:\Program Files\Microsoft Office\root\Office16\ONENOTE"
+$OUTLOOK = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK"
+$EXCEL = "C:\Program Files\Microsoft Office\root\Office16\EXCEL"
+$FINANCE = "C:\Users\cisne\OneDrive\Documents\Finances.xlsx"
+$CHROME = "C:\Program Files\Google\Chrome\Application\chrome"
+
 # Repeated functions
 Function open($this) {Start-Process $this}
 Function wait([int]$time) {Start-Sleep -Seconds $time}
@@ -6,40 +17,30 @@ Function print($this) {Write-Host $this}
 Function test {Get-Content ""}
 
 # Google search
-Function google {
-    $this = $args
-    open("https://www.google.com/search?q=$this")
-    clear
-}
+Function google {$this = $args; open("$GOOGLE$this"); clear}
 
 # Open www website
 Function www($this) {open("www.$this")}
-
 # Open fav websites
-Function reddit {clear; open("https://www.reddit.com")}
-Function dev {clear; open("https://www.dev.to")}
-Function github {clear; open("https://www.github.com")}
+Function reddit {clear; open("www.reddit.com")}
+Function dev {clear; open("www.dev.to")}
+Function github {clear; open("www.github.com")}
 
 # Open fav apps
-Function text {
-    clear; open(
-    "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Chrome Apps\Messages"
-)}
-Function music {
-    clear; open(
-    "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Chrome Apps\YouTube Music"
-)}
-Function produce {clear; open("C:\Program Files\Propellerhead\Reason 9\Reason")}
+Function text {clear; open($TEXT)}
+Function music {clear; open($MUSIC)}
+Function reason {clear; open($REASON)}
+Function chrome {clear; open($CHROME)}
 
 # Open office apps 
-Function note {clear; open("C:\Program Files\Microsoft Office\root\Office16\ONENOTE")}
-Function outlook {clear; open("C:\Program Files\Microsoft Office\root\Office16\OUTLOOK")}
-Function excel {clear; open("C:\Program Files\Microsoft Office\root\Office16\EXCEL")}
+Function note {clear; open($ONENOTE)}
+Function outlook {clear; open($OUTLOOK)}
+Function excel {clear; open($EXCEL)}
 # Open all office apps
-Function office {clear; outlook; wait(1); note; wait(1); fi}
+Function office {clear; outlook; wait(1); note; wait(1); finance}
 
-#Open files
-Function finance {open("C:\Users\cisne\OneDrive\Documents\Finances.xlsx"); clear}
+# Open files
+Function finance {open($FINANCE); clear}
 
 # Basic loop
 Function loop { 
